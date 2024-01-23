e7f1e54ca69a15a9360286c83beaa7d0af183c98
XyonaX <jonatan.vargasportillo@gmail.com>
2024-01-23 02:23:56 -0300

    se agrego SpeedInsights para probar

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,10 +1,11 @@
 import { AppRouter } from './router/AppRouter';
 import { AppTheme } from './theme/AppTheme';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export const JournalApp = () => {
 	return (
 		<AppTheme>
 			<AppRouter />
+			<SpeedInsights />
 		</AppTheme>
 	);
 };
