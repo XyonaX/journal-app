4fcabca93ca0a884c5fe456d4551069ba7709396
XyonaX <jonatan.vargasportillo@gmail.com>
2024-01-31 19:30:19 -0300

    comentamos algo del fileUpload

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -3,8 +3,8 @@ export const fileUpload = async (file) => {
 
 	const formData = new FormData();
 
-	// if (!file) throw new Error('No existe archivos a subir');
-	if (!file) return null;
+	if (!file) throw new Error('No existe archivos a subir');
+	// if (!file) return null;
 
 	formData.append('upload_preset', 'react-journal');
 	formData.append('file', file);
@@ -20,9 +20,9 @@ export const fileUpload = async (file) => {
 		const cloudResp = await resp.json();
 		return cloudResp.secure_url;
 	} catch (error) {
-		// console.log(error);
-		// throw new Error(error.message);
+		console.log(error);
+		throw new Error(error.message);
 
-		return null;
+		// return null;
 	}
 };
