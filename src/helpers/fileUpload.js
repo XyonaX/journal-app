a2881782455570e8fa02ee7ed88cf02cc26b2944
XyonaX <jonatan.vargasportillo@gmail.com>
2024-02-10 03:09:19 -0300

    pequeñas modificaciones

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -20,7 +20,6 @@ export const fileUpload = async (file) => {
 		const cloudResp = await resp.json();
 		return cloudResp.secure_url;
 	} catch (error) {
-		console.log(error);
 		throw new Error(error.message);
 
 		// return null;
