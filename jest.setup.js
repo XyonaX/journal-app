9f58a2c9fac1a5b846c39c8154616862c94cf861
XyonaX <jonatan.vargasportillo@gmail.com>
2024-02-24 20:11:18 -0300

    seguimos con el test + env

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,11 @@
 import 'whatwg-fetch';
 import 'setimmediate';
+import { getEnviroments } from './src/helpers/getEnviroments';
+
+require('dotenv').config({
+	path: '.env.test',
+});
+
+jest.mock('./src/helpers/getEnviroments', () => ({
+	getEnviroments: () => ({ ...process.env }),
+}));
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,17 +2,50 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
+import { getEnviroments } from '../helpers/getEnviroments';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const {
+	VITE_APIKEY,
+	VITE_AUTHDOMAIN,
+	VITE_PROJECTID,
+	VITE_STORAGEBUCKET,
+	VITE_MESSAGINGSENDERID,
+	VITE_APPID,
+} = getEnviroments();
+
 // Your web app's Firebase configuration
+//Dev/Prod
+// const firebaseConfig = {
+// 	apiKey: 'AIzaSyD8YKhjyOrppfxIzVN7rmvQ6MJQHr9N0pM',
+// 	authDomain: 'react-cursos-1fd8e.firebaseapp.com',
+// 	projectId: 'react-cursos-1fd8e',
+// 	storageBucket: 'react-cursos-1fd8e.appspot.com',
+// 	messagingSenderId: '656372645624',
+// 	appId: '1:656372645624:web:54fc390754e5941004450d',
+// };
+
+// console.log(import.meta.env);
+// console.log(process.env);
+//Testing
+
+// const firebaseConfig = {
+// 	apiKey: 'AIzaSyDGhyYfHvfPECJW6uBGNcJGmJdNdL0n9w8',
+// 	authDomain: 'testing-journal-4d5e5.firebaseapp.com',
+// 	projectId: 'testing-journal-4d5e5',
+// 	storageBucket: 'testing-journal-4d5e5.appspot.com',
+// 	messagingSenderId: '880291186487',
+// 	appId: '1:880291186487:web:c9d91a7e5a4a270c957cb2',
+// };
+
 const firebaseConfig = {
-	apiKey: 'AIzaSyD8YKhjyOrppfxIzVN7rmvQ6MJQHr9N0pM',
-	authDomain: 'react-cursos-1fd8e.firebaseapp.com',
-	projectId: 'react-cursos-1fd8e',
-	storageBucket: 'react-cursos-1fd8e.appspot.com',
-	messagingSenderId: '656372645624',
-	appId: '1:656372645624:web:54fc390754e5941004450d',
+	apiKey: VITE_APIKEY,
+	authDomain: VITE_AUTHDOMAIN,
+	projectId: VITE_PROJECTID,
+	storageBucket: VITE_STORAGEBUCKET,
+	messagingSenderId: VITE_MESSAGINGSENDERID,
+	appId: VITE_APPID,
 };
 
 // Initialize Firebase
