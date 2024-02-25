d9c0dac2b45763b24d76278854f0e06d84d4bfce
XyonaX <jonatan.vargasportillo@gmail.com>
2024-02-25 19:14:57 -0300

    se borraron comentarios innecesarios

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -15,30 +15,6 @@ const {
 	VITE_APPID,
 } = getEnviroments();
 
-// Your web app's Firebase configuration
-//Dev/Prod
-// const firebaseConfig = {
-// 	apiKey: 'AIzaSyD8YKhjyOrppfxIzVN7rmvQ6MJQHr9N0pM',
-// 	authDomain: 'react-cursos-1fd8e.firebaseapp.com',
-// 	projectId: 'react-cursos-1fd8e',
-// 	storageBucket: 'react-cursos-1fd8e.appspot.com',
-// 	messagingSenderId: '656372645624',
-// 	appId: '1:656372645624:web:54fc390754e5941004450d',
-// };
-
-// console.log(import.meta.env);
-// console.log(process.env);
-//Testing
-
-// const firebaseConfig = {
-// 	apiKey: 'AIzaSyDGhyYfHvfPECJW6uBGNcJGmJdNdL0n9w8',
-// 	authDomain: 'testing-journal-4d5e5.firebaseapp.com',
-// 	projectId: 'testing-journal-4d5e5',
-// 	storageBucket: 'testing-journal-4d5e5.appspot.com',
-// 	messagingSenderId: '880291186487',
-// 	appId: '1:880291186487:web:c9d91a7e5a4a270c957cb2',
-// };
-
 const firebaseConfig = {
 	apiKey: VITE_APIKEY,
 	authDomain: VITE_AUTHDOMAIN,
